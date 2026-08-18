bde330da2c5133c80d8be36fdf3e88c5a5386bb8
corodriguez <crorodriguezro@gmail.com>
2026-08-17 22:06:30 -0500

    gud: align T04 stage format

--- backport-4.9/tests/gud-kms-stage.c
+++ backport-4.9/tests/gud-kms-stage.c
@@ -125,7 +125,8 @@ int main(int argc, char **argv)
 
 	create.width = 1280;
 	create.height = 720;
-	create.bpp = 16;
+	/* T04's production gadget advertises XRGB8888 only. */
+	create.bpp = 32;
 	if (drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &create) < 0) {
 		rc = fail("DRM_IOCTL_MODE_CREATE_DUMB");
 		goto out;
@@ -154,7 +155,7 @@ int main(int argc, char **argv)
 		uint32_t offsets[4] = { 0 };
 
 		if (drmModeAddFB2(fd, create.width, create.height,
-				  DRM_FORMAT_RGB565, handles, pitches, offsets,
+				  DRM_FORMAT_XRGB8888, handles, pitches, offsets,
 				  &fb_id, 0)) {
 			rc = fail("drmModeAddFB2");
 			goto out;
